6934558b3f816646c08a1777d146f69349172586
QuocTien211 <quoctien211@gmail.com>
2024-12-14 16:24:27 +0700

    themdate

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const clock = document.getElementById("clock");
-
 function updateClock() {
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, "0");
@@ -7,6 +6,16 @@ function updateClock() {
   const seconds = String(now.getSeconds()).padStart(2, "0");
   clock.textContent = `${hours}:${minutes}:${seconds}`;
 }
-
 setInterval(updateClock, 1000);
 updateClock();
+
+const date = document.getElementById("date");
+function updateDate() {
+  const nows = new Date();
+  const days = String(nows.getDate()).padStart(2, "0");
+  const months = String(nows.getMonth() + 1).padStart(2, "0");
+  const years = String(nows.getFullYear()).padStart(2, "0");
+  date.textContent = `${days}/${months}/${years}`;
+}
+setInterval(updateDate, 1000);
+updateDate();
